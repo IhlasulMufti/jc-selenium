a6a09f1400d9b033b3be555d2d975e2a2763cd49
IhlasulMufti <ihlasul05mufti@gmail.com>
2024-10-15 17:00:08 +0800

    Day 13 - Chrome Drive + linkText

--- src/main/java/com/juaracoding/FirstSelenium.java
+++ src/main/java/com/juaracoding/FirstSelenium.java
@@ -1,27 +1,29 @@
 package com.juaracoding;
 
-import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.security.Key;
-
 public class FirstSelenium {
     public static void main(String[] args) {
 
+        // Chrome
         System.setProperty("webdriver.chrome.driver", "C:\\MyTools\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.google.com/");
 
-        // locator
+        // locator: name
         driver.findElement(By.name("q")).sendKeys("Selenium");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 
-        // delay
+        // locator: link text -> tag HTML <a>Gmail</a>
+//        driver.findElement(By.linkText("Gmail")).click();
+
+        // delay 5 detik sebelum close
         try {
-            Thread.sleep(2000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
